7ad7a716c83fca12be6866527bdadc01584df4a9
unknown <ahmedmoh21133@gmail.com>
2022-02-09 04:01:35 +0200

    create log cat D

---
+++ src/main/java/com/example/logind/WriteInLogD.java
@@ -0,0 +1,10 @@
+package com.example.logind;
+
+import android.util.Log;
+
+public class WriteInLogD {
+    private final String TAG="LOG_D";
+    public WriteInLogD(String s) {
+        Log.d(TAG,s);
+    }
+}
